c5444f759c373e753f8f2a87c26dd3c99ee4a949
Rupert Franklin <goodtryhoe@gmail.com>
2023-01-30 13:52:00 -0600

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -9,7 +9,7 @@ apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Get latest version of Portainer
-LATEST_TAG=$(curl --silent https://api.github.com/repos/portainer/portainer/releases/latest | jq -r '.tag_name')
+curl --silent https://api.github.com/repos/portainer/portainer/releases/latest
 
 # Start Portainer container
 docker run -d -p 9000:9000 --restart=always --name=portainer -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:$LATEST_TAG
